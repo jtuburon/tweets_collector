1b47ebe1e2ea3e901e0728839e6f8330cd78cdf3
Julio Mario <jtuburon@gmail.com>
2015-10-19 17:55:03 -0500

    Followers retriever added

--- followers_retriever.py
+++ followers_retriever.py
@@ -9,16 +9,10 @@ import json
 # assuming twitter_authentication.py contains each of the 4 oauth elements (1 per line)
 
 ## KIKI's TOKENS
-API_KEY = '1PWVnEFibELmjReza0191JXIJ'
-API_SECRET= 'YVIwKr2z0DjOwexufziwfiZ4OUumSOQRvrLDqVDJIagMP3jSte'
+API_KEY = '8tO9xnrbUm5qwSvv1csIEahQG'
+API_SECRET= '2i8lskOnIl6mKQwNmtdk38ZseFNnPtqNuHc6hbidicRZpsLuZ8'
 ACCESS_TOKEN = '1038975522-Y9jugcOJIMJMlBZh9gwgexCbjFD6spooVJ0AgXo'
 ACCESS_TOKEN_SECRET = 'gJqUCASC0mOyGPWjqojOQiNOJES81itA3Euv3d4K5alHI'
-
-## TEOS TOKENS
-# API_KEY= 'KVLSoYq7kirTn5HK7Y17zWg7l'
-# API_SECRET= 'ZIPTVmFjIcoU0k6miVvGj9NcusiEwVMrSieltsGTQ37iO5ulAa'
-# ACCESS_TOKEN='3957965975-nYuzNqOyNhG2ceK2RoWUVQX2eueMWLDMmpKYyIO'
-# ACCESS_TOKEN_SECRET='h57v6qpaBrxZf8Z4nzsOTzivH9wp76WcB2gBo22aGNDvi'
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -32,16 +26,20 @@ api = tweepy.API(auth_handler= auth, wait_on_rate_limit=True)
 
 counter=counter+1
 print "Counter"
+print datetime.datetime.now()
 print counter
 for c in candidates:
+	print c
 	timestamp= str(datetime.datetime.now())
 	followers=[]
-	for user in tweepy.Cursor(api.followers, screen_name= c['account']).items():
-		followers.append(user.screen_name);
+	user= api.get_user(c['account'])
+	followers_count =user.followers_count
 	f_h={
 		"history_id": counter,
 		"candidate_id": c['candidate_id'],
 		"timestamp": timestamp,
-		"followers": followers 
+		"followers_count": followers_count 
 	}
 	tweets_db.followers_history.insert(f_h);
+
+print datetime.datetime.now()
